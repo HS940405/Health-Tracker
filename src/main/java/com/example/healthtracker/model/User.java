939bc6cfc919fcfd20dd9247486afa5229fd460a
HS940405 <hansol.yang.94@gmail.com>
2022-01-15 15:00:02 -0800

    - Updated dependency - User class

--- src/main/java/com/example/healthtracker/model/User.java
+++ src/main/java/com/example/healthtracker/model/User.java
@@ -8,22 +8,48 @@ import lombok.Data;
 
 public class User {
 
-    private final String id;
-    private final String name;
-    private final String password;
-    private int weight;
-    private int heartRate;
-    private boolean cough;
-    private boolean headache;
-    private boolean stomachache;
-    private boolean nausea;
-    private int stressLevel;
+    private String id;
+    private String name;
+    private String password;
+//    private int weight;
+//    private int heartRate;
+//    private boolean cough;
+//    private boolean headache;
+//    private boolean stomachache;
+//    private boolean nausea;
+//    private int stressLevel;
 
     public User(String id, String name, String password) {
         this.id = id;
         this.name = name;
         this.password = password;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
 
 
 }
