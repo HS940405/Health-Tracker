396cc350dd0e291df87f5d68cd9d18347d42b793
HS940405 <hansol.yang.94@gmail.com>
2022-01-15 12:57:18 -0800

    modified data type of ID

--- src/main/java/com/example/healthtracker/model/User.java
+++ src/main/java/com/example/healthtracker/model/User.java
@@ -1,16 +1,16 @@
 package com.example.healthtracker.model;
 
-import java.util.UUID;
-
 import java.sql.Timestamp;
 
 import lombok.Data;
 
+@SuppressWarnings("FieldCanBeLocal")
+
 public class User {
 
-    private final UUID id;
+    private final String id;
     private final String name;
-    private String password;
+    private final String password;
     private int weight;
     private int heartRate;
     private boolean cough;
@@ -19,12 +19,11 @@ public class User {
     private boolean nausea;
     private int stressLevel;
 
-    public User(UUID id, String name, String password) {
+    public User(String id, String name, String password) {
         this.id = id;
         this.name = name;
         this.password = password;
-        this.weight = weight;
-        this.heartRate = heartRate;
     }
+
 
 }
